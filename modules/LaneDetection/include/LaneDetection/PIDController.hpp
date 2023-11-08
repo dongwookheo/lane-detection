58bc14356fa7578054e82ad2d9af0f5ffa61e0c4
nahye03 <020977@naver.com>
2023-11-09 00:14:48 +0900

    Changed constructor and fix wrong name

--- modules/LaneDetection/include/LaneDetection/PIDController.hpp
+++ modules/LaneDetection/include/LaneDetection/PIDController.hpp
@@ -11,12 +11,8 @@ namespace Xycar{
 class PIDController
 {
 public:
-    PIDController(PREC p_gain, PREC i_gain, PREC d_gain)
-    {
-        proportional_gain_(p_gain);
-        integral_gain_(i_gain);
-        differential_gain_(d_gain);
-    }
+    PIDController(PREC p_gain, PREC i_gain, PREC d_gain);
+    
 
     PREC getControlOutput(int32_t error);
 
